1a719a27b5f99ee940135e8b15f7917456992c27
milo9301 <milo199301@outlook.com>
2024-09-19 10:59:10 +0800

    Initial commit

---
+++ src/main/java/org/taibiex/taskservice/TaibiexTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package org.taibiex.taskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaibiexTaskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaibiexTaskServiceApplication.class, args);
+    }
+
+}
